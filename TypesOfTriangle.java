2d1427626a404c477ce72cf573419b9008361713
radhika0104 <radhikaagrawal0104@gmail.com>
2026-04-09 22:53:16 +0530

    Implement triangle type determination logic

---
+++ TypesOfTriangle.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String triangleType(int[] nums) {
+        int a = nums[0];
+        int b = nums[1];
+        int c = nums[2];
+        // for (int ele : nums) {
+            if (((a + b > c) && (b + c > a)) && (a + c > b)) {
+                if (a == b && b == c && c==a) {
+                    return "equilateral";
+                }
+                else if((a==b || b==c) || c==a){
+                    return "isosceles";
+                } 
+                else
+                    return "scalene";
+            }
+        return "none";
+    }
+}
