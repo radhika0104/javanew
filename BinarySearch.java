80349522c01a87aa7af2aaa22df76f976d6fc975
radhika0104 <radhikaagrawal0104@gmail.com>
2026-04-13 22:52:29 +0530

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int ans;
+        while (left <= right) {
+            int mid = left + (right-left) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                return ans;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else
+                right = mid - 1;
+
+        }
+        // ans=left;
+        return -1;
+
+    }
+}
