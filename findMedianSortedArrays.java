71d48f1d6e62036544366b17972cc451112ff2fa
radhika0104 <radhikaagrawal0104@gmail.com>
2026-04-16 22:09:25 +0530

    median of two sorted arrays

---
+++ findMedianSortedArrays.java
@@ -0,0 +1,34 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+       
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int left = (n1 + n2 + 1) / 2;
+        int right = (n1 + n2 + 2) / 2;
+        
+        
+        return (getKth(nums1, 0, nums2, 0, left) + 
+                getKth(nums1, 0, nums2, 0, right)) / 2.0;
+    }
+
+    private int getKth(int[] nums1, int start1, int[] nums2, int start2, int k) {
+        
+        if (start1 >= nums1.length) return nums2[start2 + k - 1];
+        if (start2 >= nums2.length) return nums1[start1 + k - 1];
+        
+        
+        if (k == 1) return Math.min(nums1[start1], nums2[start2]);
+
+        
+        int mid1 = (start1 + k / 2 - 1 < nums1.length) ? nums1[start1 + k / 2 - 1] : Integer.MAX_VALUE;
+        int mid2 = (start2 + k / 2 - 1 < nums2.length) ? nums2[start2 + k / 2 - 1] : Integer.MAX_VALUE;
+
+        
+        if (mid1 < mid2) {
+            return getKth(nums1, start1 + k / 2, nums2, start2, k - k / 2);
+        } else {
+            return getKth(nums1, start1, nums2, start2 + k / 2, k - k / 2);
+        }
+     
+    }
+} 
