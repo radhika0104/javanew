2ca178dc79d5cfda0107d47b42c9b2f99c1ff838
radhika0104 <radhikaagrawal0104@gmail.com>
2026-05-16 13:26:11 +0530

    Implement addSpaces method to insert spaces

---
+++ 2109.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String addSpaces(String s, int[] spaces) {
+        StringBuilder sb=new StringBuilder("");
+        int j=0;
+        int i=0;
+        while(i<s.length()){
+            if(j<spaces.length && i==spaces[j]){
+                sb.append(" ");
+                j++;
+            }
+            sb.append(s.charAt(i));
+            i++;
+        }
+        return sb.toString();
+    }
+}
