1292e3d075f2c6e2e641663c175a642bc68a007b
radhika0104 <radhikaagrawal0104@gmail.com>
2026-06-24 20:02:07 +0530

    Add triangleNumber method to count triangles
    Implement a method to count valid triangle triplets in an array.

---
+++ lc_611.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int count=0;
+        for(int i=0;i<nums.length-2;i++){
+            for(int j=i+1;j<nums.length-1;j++){
+                for(int k=j+1;k<nums.length;k++){
+                    if(nums[i]+nums[j]>nums[k])
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
