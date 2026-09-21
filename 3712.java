56fcab8686fa2800ddf3e39ebef63e6b1a15f278
radhika0104 <radhikaagrawal0104@gmail.com>
2026-09-21 22:44:34 +0530

    Implement sumDivisibleByK method in Solution class

---
+++ 3712.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int sumDivisibleByK(int[] nums, int k) {
+        int sum=0;
+        int[] arr= new int[101];
+        for(int i=0;i<nums.length;i++){
+            arr[nums[i]]++;
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%k==0)
+            sum+=arr[i]*i;
+        }
+        return sum;
+    }
+}
